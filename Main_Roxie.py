8b7c8161c3c8308866e3216624d63692f2c60ed6
ASUS <longthkgcs18852@fpt.edu.vn>
2022-03-11 17:05:33 +0700

    Create layout

--- Main_Roxie.py
+++ Main_Roxie.py
@@ -3,8 +3,10 @@ import datetime
 
 import pyttsx3
 import speech_recognition as sr
-from multiprocessing import Process
 import warnings
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow
+from Roxy-Layout import Ui_MainWindow
 
 
 from Roxie import Roxie1  # get function Roxie1 from Roxie.py
@@ -60,3 +62,23 @@ while True:
     ai_mouth.setProperty('voice', voices[1].id)  # voice AI: 0(Male), 1(Female)
     ai_mouth.say(ai_brain)
     ai_mouth.runAndWait()
+
+
+
+
+
+class MainWindow:
+    def __init__(self):
+        self.main_win = QMainWindow()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self.main_win)
+
+
+    def show(self):
+        self.main_win.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
